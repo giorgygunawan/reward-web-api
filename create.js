8ff22fc7638ce4e12aa6304b5a04c3c097e05205
giorgy gunawan <giorgy@styletheory.co>
2019-08-18 19:48:48 +0700

    make `create` reward API

---
+++ create.js
@@ -0,0 +1,30 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+export async function main(event, context) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: "rewards",
+    Item: {
+      user_id: event.requestContext.identity.cognitoIdentityId,
+      reward_id: uuid.v1(),
+      image: data.image,
+      title: data.title,
+      vendor_name: data.vendor.vendor_name,
+      vendor_image: data.vendor.vendor_image,
+      type: data.redemption_type,
+      flash_sale_begin: data.flash_sale_begin,
+      flash_sale_end: data.flash_sale_end,
+      createdAt: Date.now()
+    }
+  };
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
